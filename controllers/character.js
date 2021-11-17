9a3214a7248dbd70eb7b8fafcb29a4c4e6b59e16
LindZCoding <Lindzgaming@gmail.com>
2021-11-17 07:48:53 -0800

    When you search for a character you can click on their name and it will take you to a page for the characters informationgit status

--- controllers/character.js
+++ controllers/character.js
@@ -6,7 +6,7 @@ router.get('/characterResults', (req, res)=>{
     let characterSearch = req.query.characterSearch
     axios.get(`https://api.jikan.moe/v3/search/character?q=${characterSearch}`)
     .then(apiData => {
-        console.log("this is CHAR APIDATA:", apiData.data.results)
+        // console.log("this is CHAR APIDATA:", apiData.data.results)
         let characterResults = apiData.data.results
         res.render('characterResults', {characterResults: characterResults})
     })
@@ -16,16 +16,16 @@ router.get('/characterResults', (req, res)=>{
 })
 
 router.get('/:mal_id', (req, res) => {
-    let characterSearch = req.params.mal_id
-    console.log("this is the CHAR MAL ID:", characterSearch)
-    axios.get(`https://api.jikan.moe/v3/search/character?q=${characterSearch}`)
+    let characterSearched = req.params.mal_id
+    console.log("this is the CHAR MAL ID:", characterSearched)
+    axios.get(`https://api.jikan.moe/v3/character/${characterSearched}/`)
     .then(idResults => {
-        // console.log("this is single CHAR APIDATA:", idResults.data)
-        let animeId = req.params.mal_id
+        console.log("this is single CHAR APIDATA:", idResults.data)
+        // let mal_id = req.params.mal_id
         let name = idResults.data.name
         let image_url = idResults.data.image_url
 
-        res.render('characterDetail', {name: name, title, image_url: image_url, animeId: animeId})
+        res.render('characterDetail', {name: name, image_url: image_url})
     })
 })
 
